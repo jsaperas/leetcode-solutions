063a3c30651588c24eb265555f625c183988fc7c
jsaperas <saperas.jordi@gmail.com>
2021-12-02 07:51:00 -0600

    Update second-highest-salary.sql

--- second-highest-salary/second-highest-salary.sql
+++ second-highest-salary/second-highest-salary.sql
@@ -1,9 +1,4 @@
 /* Write your PL/SQL query statement below */
-SELECT 
-    MAX(CASE WHEN SalaryRank = 2 THEN salary ELSE NULL END) AS SecondHighestSalary
-FROM (
-    SELECT 
-        salary,
-        RANK() OVER (ORDER BY salary DESC) AS SalaryRank
-    FROM Employee
-)
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee)
